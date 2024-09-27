a6fa93ec77a3e0dac4f0dc6924c49df4e10decbb
cullen <palisocericson87@gmail.com>
2024-09-27 10:37:01 +0800

    implementing services (landing page)

--- frontend/src/components/LandingPage/Services.jsx
+++ frontend/src/components/LandingPage/Services.jsx
@@ -1,16 +1,51 @@
 import React from 'react'
 
+// "Delve into a variety of services designed to elevate your brand presence to the ultimate level."
+
+// "Discover a comprehensive suite of web development solutions designed to elevate your online presence."
+
 const Services = () => {
   return (
     <div>
-      <h1>Our Services</h1>
+      <h1 className='font-medium'>Our Services</h1>
 
-      <div>
+      <div className='grid grid-cols-1 lg:grid-cols-2'>
         <div className='left'>
-          <h3></h3>
-          <p></p>
+          <h1 className='text-5xl leading-tight'>
+            Delve into a variety of services designed to elevate your brand
+            presence to the ultimate level.
+          </h1>
         </div>
-        <div className='right'></div>
+        <div className='right grid grid-cols-2 gap-x-3'>
+          <div>
+            <h2 className='bg-base-200 p-5 '>Virtual Assistance</h2>
+            <ul>
+              <li>
+                <a href=''>qe</a>
+              </li>
+              <li>
+                <a href=''>qe</a>
+              </li>
+              <li>
+                <a href=''>qe</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className='bg-base-200 p-5'>Recruitment Services</h2>
+            <ul>
+              <li>
+                <a href=''>qe</a>
+              </li>
+              <li>
+                <a href=''>qe</a>
+              </li>
+              <li>
+                <a href=''>qe</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
--- frontend/src/pages/userPages/Home.jsx
+++ frontend/src/pages/userPages/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Hero from '../../components/Hero'
 import WhyChooseSkillBridge from '../../components/LandingPage/WhyChooseSkillBridge'
 import BridgingTalent from '../../components/LandingPage/BridgingTalent'
+import Services from '../../components/LandingPage/Services'
+
 const Home = () => {
   return (
     <div className='container'>
       <Hero />
       <WhyChooseSkillBridge />
       <BridgingTalent />
+      <Services />
     </div>
   )
 }
