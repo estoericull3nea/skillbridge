83a7de1e9dfa42e4459a703c8f63a6503618b61b
estoericull3nea <palisocericson87@gmail.com>
2024-10-09 22:35:44 +0800

    implementing zoom api (REST)

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import { fileURLToPath } from 'url'
 import bookRouter from './api/routes/booking.route.js'
 import authRouter from './api/routes/auth.route.js'
 import userRouter from './api/routes/user.route.js'
+import meetingRouter from './api/routes/meeting.route.js'
 // import loginHistoryRouter from './api/routes/loginHistory.route.js'
 // import feedbackRouter from './api/routes/feedback.route.js'
 // import contactRouter from './api/routes/contact.route.js'
@@ -57,6 +58,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/book', bookRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/meetings', meetingRouter)
 // app.use('/api/feedbacks', feedbackRouter)
 // app.use('/api/contacts', contactRouter)
 // app.use('/api/logins/history', loginHistoryRouter)
