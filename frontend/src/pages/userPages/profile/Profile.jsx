4e9eaf90c879a1be7413d84e2e2c798723058435
estoericull3nea <palisocericson87@gmail.com>
2024-10-23 23:25:15 +0800

    updated

--- frontend/src/pages/userPages/profile/Profile.jsx
+++ frontend/src/pages/userPages/profile/Profile.jsx
@@ -60,9 +60,21 @@ const Profile = () => {
                 </div>
               </div>
 
-              <div className=' text-end flex justify-end'>
-                <div className='mb-3 px-3 py-2 text-end bg-white shadow max-w-max rounded-full '>
-                  <IoMdNotificationsOutline />
+              <div className='mb-3 px-3 py-2 text-end bg-white shadow max-w-max rounded-full flex items-center justify-center '>
+                <div className='dropdown dropdown-hover dropdown-end'>
+                  <IoMdNotificationsOutline tabIndex={0} role='button' />
+
+                  <ul
+                    tabIndex={0}
+                    className='dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow'
+                  >
+                    <li>
+                      <a>Item 1</a>
+                    </li>
+                    <li>
+                      <a>Item 2</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
