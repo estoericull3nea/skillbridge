098151fa298245f8dff5d1449a188e31bb8c2cbd
Ericson Palisoc <palisocericson87@gmail.com>
2024-12-16 10:11:35 +0800

    updating tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -17,7 +18,7 @@ export default {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: ['light'],
   },
