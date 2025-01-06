93f77fc046a5c5442547ff47a2689fc4374c4a6f
Ericson Palisoc <palisocericson87@gmail.com>
2025-01-07 01:43:33 +0800

    login page back to import rather process

--- frontend/src/pages/userPages/Login.jsx
+++ frontend/src/pages/userPages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  console.log(process.env.VITE_PROD_BACKEND_URL)
+  console.log(import.meta.env.VITE_PROD_BACKEND_URL)
 
   const loginMutation = useMutation({
     mutationFn: async (loginData) => {
